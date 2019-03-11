93b4b9f38aaaabc86c03f6f1b71a2225869777e3
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-03-10 21:40:57 -0500

    eliminación de archivos

--- winerama/winerama/settings.py
+++ winerama/winerama/settings.py
@@ -82,10 +82,22 @@ WSGI_APPLICATION = 'winerama.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wine',
+        'USER': 'wine',
+        'PASSWORD': 'wine',
+        'HOST': '172.17.0.2',
+        'PORT': '5432'
     }
 }
 
